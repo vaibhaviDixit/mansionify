11c7c0135694863a146db900443636f9464f2555
Vaibhavi Dixit <88642745+vaibhaviDixit@users.noreply.github.com>
2024-02-14 09:55:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -73,6 +73,17 @@ app.use((req, res, next) => {
         'frame-src https://console.dialogflow.com/',
         "default-src 'none'; font-src 'self' data:; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
     );
+
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+    
     next();
 });
 
